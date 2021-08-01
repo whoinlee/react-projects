2edd3ab40696be4dc77b3b79642bf5e50a14e0fb
Whoin Lee <WhoINsAIR@Whoins-Air.home>
2021-08-01 13:13:31 -0400

    shopping-cart updated

--- shopping-cart/src/App.tsx
+++ shopping-cart/src/App.tsx
@@ -3,7 +3,7 @@ import { useQuery } from 'react-query';
 //-- Components
 import Cart from './Cart/Cart';
 import Item from './Item/Item';
-//
+//--
 import Grid from '@material-ui/core/Grid';
 import Badge from '@material-ui/core/Badge';
 import Drawer from '@material-ui/core/Drawer';
@@ -19,23 +19,55 @@ export type CartItemType = {  //-- ** export ** --//
   image: string;
   price: number;
   title: string;
-  amount: number;
+  amount: number; //-- added
 }
 
 
 //-- fetching : fake store API
 const STORE_API = 'https://fakestoreapi.com/products';
+
 const getProducts = async (): Promise<CartItemType[]> => 
 await (await fetch(STORE_API)).json();
 
 const App = () => {
   const [cartOpen, setCartOpen] = useState(false);
   const [cartItems, setCartItems] = useState([] as CartItemType[]);
-  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts);
-  // console.log(data);
+  const { data, isLoading, error } = useQuery<CartItemType[]>('products', getProducts); //-- useQuery를 쓰면 useEffect를 안써도 알아서 한 번만 로딩????
+  console.log(`data: ${data}`);
+  // console.log(`isLoading: ${isLoading}`);
+  // console.log(`error: ${error}`);
+
+  // const result = useQuery<CartItemType[]>('products', getProducts);
+  // console.log(`result.isLoading: ${result.isLoading}`);
+  // for (const property in result) {
+  //   console.log(`${property}: ${result[property]}`);
+  // }
+  //---
+  // status: success
+  // isLoading: false
+  // isSuccess: true
+  // isError: false
+  // isIdle: false
+  // data: [object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object],[object Object]
+  // dataUpdatedAt: 1627836461437
+  // error: null
+  // errorUpdatedAt: 0
+  // failureCount: 0
+  // isFetched: true
+  // isFetchedAfterMount: true
+  // isFetching: false
+  // isLoadingError: false
+  // isPlaceholderData: false
+  // isPreviousData: false
+  // isRefetchError: false
+  // isStale: true
+  // refetch: function () { [native code] }
+  // remove: function () { [native code] }
+
   
   //-- accumulator & item
   const getTotalItems = (items:CartItemType[]) => items.reduce((accm: number, item) => accm + item.amount, 0);  //-- implicit return
+  //
   const handleAddToCart = (clickedItem:CartItemType) => {
     setCartItems(prevCartItems => {
       //-- is the item already in the cart?
@@ -78,6 +110,7 @@ const App = () => {
         </Badge>
       </StyledButton>
       <Grid container spacing={3}>
+      {/* The part after the question mark is only executed if data exists, need???? here???? */}
         {data?.map(item => (
           <Grid item key={item.id} xs={12} sm={4} >
             <Item item={item} handleAddToCart={handleAddToCart} />
