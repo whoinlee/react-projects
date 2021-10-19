e9cc470b56315482fac0ec3ebc2b2db6c54b5209
Whoin Lee <whoinlee@gmail.com>
2021-10-18 20:46:26 -0400

    p21 start

---
+++ p21_weather-dataviz/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="appHeader">
+      </header>
+    </div>
+  );
+}
+
+export default App;
